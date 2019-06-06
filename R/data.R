869ccac99604303d1bda11a37ccd83fad164e515
arnfinn <arnfinn.steindal@gmail.com>
2019-06-06 12:48:09 +0200

    converted barn, kols and dagkir to geojson and r data

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Map from the day surgery healthcare atlas
+#'
+#' @docType data
+#' @keywords map
+#' @name dagkir
+NULL
+
+#' Map from the children healthcare atlas
+#'
+#' @docType data
+#' @keywords map
+#' @name barn
+NULL
+
+#' Map from the COPD healthcare atlas
+#'
+#' @docType data
+#' @keywords map
+#' @name kols
+NULL
---
+++ tests/development/makegeojson.R
@@ -0,0 +1,20 @@
+convert_all_shp <- function(){
+  
+  name <- "barn"
+  maps::shp2geojson(shapefile = name, folder = "shp", geojson = paste0("geojson/", name))
+  tmp <- geojsonio::geojson_read(paste0("geojson/", name, ".geojson"), what = "sp")
+  barn <- shinymap::utm33_to_leaflet(map = tmp)
+  usethis::use_data(barn, overwrite = TRUE)
+
+  name <- "kols"
+  maps::shp2geojson(shapefile = name, folder = "shp", geojson = paste0("geojson/", name), reduce_size = FALSE, amount = 1.0)
+  tmp <- geojsonio::geojson_read(paste0("geojson/", name, ".geojson"), what = "sp")
+  kols <- shinymap::utm33_to_leaflet(map = tmp)
+  usethis::use_data(kols, overwrite = TRUE)
+
+  name <- "dagkir"
+  maps::shp2geojson(shapefile = name, folder = "shp", geojson = paste0("geojson/", name))
+  tmp <- geojsonio::geojson_read(paste0("geojson/", name, ".geojson"), what = "sp")
+  dagkir <- shinymap::utm33_to_leaflet(map = tmp)
+  usethis::use_data(dagkir, overwrite = TRUE)
+}
