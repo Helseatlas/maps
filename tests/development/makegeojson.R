b2ee4b97ef1fb31fc7470d0b92f81f88104d8df4
arnfinn <arnfinn.steindal@gmail.com>
2019-06-07 10:36:12 +0200

    converted eldre.shp and nyfodt.shp to geojson and rda

--- tests/development/makegeojson.R
+++ tests/development/makegeojson.R
@@ -7,7 +7,7 @@ convert_all_shp <- function(){
   usethis::use_data(barn, overwrite = TRUE)
 
   name <- "kols"
-  maps::shp2geojson(shapefile = name, folder = "shp", geojson = paste0("geojson/", name), reduce_size = FALSE, amount = 1.0)
+  maps::shp2geojson(shapefile = name, folder = "shp", geojson = paste0("geojson/", name), reduce_size = FALSE)
   tmp <- geojsonio::geojson_read(paste0("geojson/", name, ".geojson"), what = "sp")
   kols <- shinymap::utm33_to_leaflet(map = tmp)
   usethis::use_data(kols, overwrite = TRUE)
@@ -17,6 +17,18 @@ convert_all_shp <- function(){
   tmp <- geojsonio::geojson_read(paste0("geojson/", name, ".geojson"), what = "sp")
   dagkir <- shinymap::utm33_to_leaflet(map = tmp)
   usethis::use_data(dagkir, overwrite = TRUE)
+
+  name <- "eldre"
+  maps::shp2geojson(shapefile = name, folder = "shp", geojson = paste0("geojson/", name), reduce_size = FALSE)
+  tmp <- geojsonio::geojson_read(paste0("geojson/", name, ".geojson"), what = "sp")
+  eldre <- shinymap::utm33_to_leaflet(map = tmp)
+  usethis::use_data(eldre, overwrite = TRUE)
+  
+  name <- "nyfodt"
+  maps::shp2geojson(shapefile = name, folder = "shp", geojson = paste0("geojson/", name), reduce_size = TRUE, amount = 0.5)
+  tmp <- geojsonio::geojson_read(paste0("geojson/", name, ".geojson"), what = "sp")
+  nyfodt <- shinymap::utm33_to_leaflet(map = tmp)
+  usethis::use_data(nyfodt, overwrite = TRUE)
 
   name <- "dagkir2"
   maps::shp2geojson(shapefile = name, folder = "shp", geojson = paste0("geojson/", name))
